7f78df4095535b89b5f5eec8e7f71f81f3da2216
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2022-04-11 10:43:21 +0800

    install telescope

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,9 @@
                             <a class="nav-link {{ request()->routeIs('books.index') ? 'active' : '' }}"
                                 href="{{ route('books.index') }}">Buku</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" target="_blank" href="{{ route('telescope') }}">Telescope</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -72,7 +75,7 @@
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                 document.getElementById('logout-form').submit();">
+                                                                                         document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
